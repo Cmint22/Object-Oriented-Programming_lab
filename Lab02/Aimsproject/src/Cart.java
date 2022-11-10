7f9395e0044bae569a6bcc441cf84b9726183480
Cmint22 <you@example.com>
2022-11-10 19:20:03 +0700

    first commit

--- Lab02/Aimsproject/src/Cart.java
+++ Lab02/Aimsproject/src/Cart.java
@@ -1,24 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 public class Cart {
 	public static final int MAX_NUMBERS_ORDERED = 20;
 	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
-	public DigitalVideoDisc[] getItemsOrdered() {
-		return itemsOrdered;
+	private int qtyOrdered= 0;
+	private float ttCost;
+	
+	public int getQtyOrdered() {
+		return qtyOrdered;
 	}
-	public void setItemsOrdered(DigitalVideoDisc itemsOrdered[]) {
-		this.itemsOrdered = itemsOrdered;
+	public void setQtyOrdered(int qtyOrdered) {
+		this.qtyOrdered = qtyOrdered;
 	}
-	public char[] totalCost() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	public void addDigitalVideoDisc(DigitalVideoDisc dvd1) {
-		// TODO Auto-generated method stub
+
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if(qtyOrdered < MAX_NUMBERS_ORDERED) {
+			this.itemsOrdered[this.qtyOrdered++] = disc;
+			System.out.printf("The disc %s has been added.\n", disc.getTitle());
+		}
+		else System.out.println("The order is almost full.\n");
 		
+	}
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		if(qtyOrdered > 0) {
+			for(int i = 0; i < qtyOrdered; i++) {
+				if(itemsOrdered[i] == disc) {
+					for(int j = i; j < qtyOrdered - 1; j++) {
+						itemsOrdered[j] = itemsOrdered[j+1];
+					}
+					itemsOrdered[--qtyOrdered] = null;
+					break;
+				}
+			}
+			System.out.printf("The disc %s has been removed.\n", disc.getTitle());
+		}
+		else System.out.println("The order is almost empty.\n");
+	}
+	
+	public float totalCost() {
+		ttCost = 0;
+		for(int i = 0; i < qtyOrdered; i++) {
+			ttCost += itemsOrdered[i].getCost();
+			//can create precision number that affect the last result.
+		}
+		return ttCost;
 	}
 }
